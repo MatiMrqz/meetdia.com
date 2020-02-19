e426d9410518804192ce5e21623ec00527608625
MatiMrqz <mati.mrqz@gmail.com>
2020-02-19 10:00:09 -0300

    Creada entidad Post

---
+++ meetdia.com/src/main/java/com/edu/egg/entidades/Post.java
@@ -0,0 +1,78 @@
+package com.edu.egg.entidades;
+
+import com.edu.egg.enumeraciones.Categoria;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid", strategy="uuid2")
+    private String id;
+    private String titulo;
+    private String descripcion;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fecha_publicacion;
+    
+    @ManyToOne
+    private Persona persona;
+    
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha_publicacion() {
+        return fecha_publicacion;
+    }
+
+    public void setFecha_publicacion(Date fecha_publicacion) {
+        this.fecha_publicacion = fecha_publicacion;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+}
