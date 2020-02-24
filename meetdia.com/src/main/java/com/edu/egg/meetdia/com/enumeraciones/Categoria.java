3a1116f727859b3d55116edd1202be36fafaf1e7
MatiMrqz <mati.mrqz@gmail.com>
2020-02-24 02:24:58 -0300

    Cambio en jerarquia de carpetas... Para coincidir con branch heroku

--- meetdia.com/src/main/java/com/edu/egg/meetdia/com/enumeraciones/Categoria.java
+++
@@ -1,6 +0,0 @@
-package com.edu.egg.meetdia.com.enumeraciones;
-
-public enum Categoria {
-MUSICA,FOTOGRAFIA,CINEYVIDEO,ILUMINACION,GUIONES,ANIMACION
-}
-//LISTO
--- meetdia.com/src/main/java/com/edu/egg/meetdia/com/enumeraciones/Tipo.java
+++
@@ -1,7 +0,0 @@
-package com.edu.egg.meetdia.com.enumeraciones;
-
-public enum Tipo {
-	VIDEO,
-	IMAGEN,
-	AUDIO;
-}
