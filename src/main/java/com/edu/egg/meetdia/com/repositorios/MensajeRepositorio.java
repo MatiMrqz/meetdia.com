9a27680d2f642e1a758014c1cc757d176be25499
MatiMrqz <mati.mrqz@gmail.com>
2020-03-08 13:13:33 -0300

    Arreglado mensajeservicio y repositorio...

--- src/main/java/com/edu/egg/meetdia/com/repositorios/MensajeRepositorio.java
+++ src/main/java/com/edu/egg/meetdia/com/repositorios/MensajeRepositorio.java
@@ -23,8 +23,10 @@ public interface MensajeRepositorio extends JpaRepository<Mensaje,String> {
 	@Query("SELECT a FROM Mensaje a WHERE a.contenido= :contenido")
 	public Mensaje buscarContenido(@Param("contenido") String contenido);
 
-	@Query("SELECT a FROM Mensaje a WHERE a.emisor =: emisor AND a.receptor =: receptor ORDER BY a.fecha")
-	public List<Mensaje> buscarMensajeEmisorReceptor(@Param("emisor") Persona emisor,
-			@Param("receptor") Persona receptor);
+	@Query("SELECT m FROM Mensaje m WHERE (m.emisor= :pemisor AND m.receptor= :preceptor) OR (m.receptor = :pemisor AND m.emisor= :preceptor) ORDER BY m.fecha ASC")
+	public List<Mensaje> buscarMensaje2personas(@Param("pemisor") Persona emisor, @Param("preceptor") Persona receptor);
+        
+        @Query("SELECT m FROM Mensaje m where (m.receptor = :user and m.emisor != :user) or (m.emisor = :user and m.receptor != :user) order by m.fecha ASC")
+        public List<Mensaje> chatsde(@Param("user") Persona usuario);
 
 }
